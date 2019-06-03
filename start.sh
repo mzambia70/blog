3fee19bfe19477e224f9d39139c30a43c43a3c27
mzambia70 <mzambia70@gmail.com>
2019-06-03 15:45:34 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY=Elchapo
-export MAIL_USERNAME= mzambia70@gmail.com
-export MAIL_PASSWORD= mzambia
+export MAIL_USERNAME=mzambia70@gmail.com
+export MAIL_PASSWORD=mzambia
 
 python3.6 manage.py server
